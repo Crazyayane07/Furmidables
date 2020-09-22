cc0db505aab18739cea5093f062b2f5420ac1d66
crazyayane123 <kar.lenczewska@gmail.com>
2020-09-22 14:57:51 +0200

    Init

---
+++ Source/Furmidables/FurmidablesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FurmidablesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FURMIDABLES_API AFurmidablesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
