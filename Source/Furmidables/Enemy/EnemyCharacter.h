f0518606ee97f0bfa62e28c9a7686f89e5423e39
crazyayane123 <kar.lenczewska@gmail.com>
2020-09-29 15:02:13 +0200

    Added basic enemy class

---
+++ Source/Furmidables/Enemy/EnemyCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+class UDamageHandlerComp;
+
+UCLASS()
+class FURMIDABLES_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemyCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UDamageHandlerComp* DamageHandlerComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
